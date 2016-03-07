40bd677e34f0200d697e331de4d98564818faab9
liam4 <towerofnix@gmail.com>
2016-03-07 19:39:00 -0400

    bad code is less bad

--- go.sh
+++ go.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 printf 'compile...'
-nearleyc grammar.ne -o grammar.js
+
+# meh this code is bad
+# it assumes you already have nearley globally installed
+#nearleyc grammar.ne -o grammar.js
+node_modules/nearley/bin/nearleyc.js grammar.ne -o grammar.js # 9001 TIMES BETTER!!!1!
+# oops went over the 80 character limit ^D^
+
 echo 'done'
-printf 'run script...'
+
+# lol some of this is unnecessary
+#printf 'run script...'
 node index.js
-echo 'done'
+#echo 'done'
--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,6 @@
 // * doesn't use index.js
 // * no shell hacks
 // * development shouldn't require writing to files
+
+// lol I'm not going to do this until Hardmath123 gets back to me about
+// using nearleyc w/ node.js :)
