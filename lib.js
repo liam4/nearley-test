b65a88a72cc39913e7a143947557436bde95e288
liam4 <towerofnix@gmail.com>
2016-03-07 16:40:38 -0400

    lib.js comments

--- lib.js
+++ lib.js
@@ -1,12 +1,26 @@
+// Variable class -------------------------------------------------------------
+// * this should never *ever* be accessed through anywhere except set/get
+//   variable functions
+// * takes one paramater, value, which is stored in inst.value and represents
+//   the value of the variable
 export class Variable {
   constructor(value) {
     this.value = value;
   }
 }
 
+// Base token class -----------------------------------------------------------
+// * doesn't do anything
+// * use x instanceof Token to check if x is any kind of token
+
 export class Token {
   constructor() {}
 }
+
+// Function token class -------------------------------------------------------
+// * takes one paramater, fn, which is stored in inst.fn and represents the
+//   function that will be called
+// * use inst.__call__ to call the function (with optional arguments)
 
 export class FunctionToken extends Token {
   constructor(fn) {
@@ -15,15 +29,18 @@ export class FunctionToken extends Token {
   }
 
   __call__(args) {
+    // Call this function. By default uses defaultCall, but can be overriden
+    // by subclasses.
     return defaultCall(this, args);
   }
+}
+
+// Call function --------------------------------------------------------------
+
+export function call(fn, args) {
+  return fn['__call__'](args);
 }
 
 export function defaultCall(fnToken, args) {
   return fnToken.fn(args);
 }
-
-export function call(fn, args) {
-  return fn['__call__'](args);
-};
-
