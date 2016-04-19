d30162031a4ae15673ad687e75bbbbe5db611c72
liam4 <towerofnix@gmail.com>
2016-04-19 14:54:12 -0300

    test

--- tests/tests.js
+++ tests/tests.js
@@ -1,1 +1,24 @@
+'use strict';
+
 // idk, 109C wants me to make this.
+
+const { run } = require('../dist/run');
+
+const test = function(code, assume) {
+  const oldLog = console.log;
+  const out = [];
+  console.log = function(...args) {
+    out.push(args);
+    oldLog('[test]', args);
+  }
+  run(code);
+  console.log = oldLog;
+  if (!assume(out)) {
+    console.log('DERP');
+  }
+};
+
+test('print("hello!");', function(res) {
+  console.log('test');
+  console.log('result:', res);
+});
