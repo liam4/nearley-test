5ef3c6beee78285c5a5956f231b380cfc39cd432
Alex Bates <16batesa@gmail.com>
2016-04-23 18:26:21 +0100

    Add eslint & fix up package.json & README.md

--- src/builtin_lib/hello.js
+++ src/builtin_lib/hello.js
@@ -1,6 +1,6 @@
-var lib = require('../lib');
+const lib = require('../lib')
 
-export var lol = new lib.LFunction(function() {
-  console.log("Lololol!");
-  return lib.toLNumber(1337);
-});
+export let lol = new lib.LFunction(function() {
+  console.log('Lololol!')
+  return lib.toLNumber(1337)
+})
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 /*
 var code = `
+=======
+const code = `
+>>>>>>> Stashed changes
 
 http => use("http.js");
 server => http.server(fn(req, res) {
@@ -8,41 +11,44 @@ server => http.server(fn(req, res) {
 });
 server.listen(8080);
 
-`;
+`
 
-require('source-map-support').install();
-var nearley = require('nearley');
-var grammar = require('./grammar');
-var run = require('./run');
+require('source-map-support').install()
+const nearley = require('nearley')
+const grammar = require('./grammar')
+const run = require('./run')
 
-var parser = new nearley.Parser(grammar.ParserRules, grammar.ParserStart);
+const parser = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
 
 try {
-  var result = run.run(code);
+  const result = run.run(code)
   // console.log('result:', result);
   // console.log('all ASTs:', JSON.stringify(asts, null, 1));
   // console.log('AST:', JSON.stringify(ast, null, 1));
 } catch(e) {
-  if (e.offset) {
-    console.error("Syntax error on character " + e.offset);
-    var i = e.offset;
-    var line = '';
-    while (code[i] && code[i] !== '\n') {
-      line = code[i] + line;
-      i--;
+  if(e.offset) {
+    console.error('Syntax error on character ' + e.offset)
+    let i = e.offset
+    let line = ''
+    while(code[i] && code[i] !== '\n') {
+      line = code[i] + line
+      i--
     }
-    var lineStartOff = i + 1;
-    i = e.offset + 1;
-    while (code[i] && code[i] !== '\n') {
-      line = line + code[i];
-      i++;
+    let lineStartOff = i + 1
+    i = e.offset + 1
+    while(code[i] && code[i] !== '\n') {
+      line = line + code[i]
+      i++
     }
-    console.error('line:\n' + line + '\n' + ' '.repeat(e.offset - lineStartOff) + '^');
-  } else if (e.expr) {
-    console.error(e.stack);
-    console.error(e.expr);
+    console.error(`line:
+${line}
+${' '.repeat(e.offset - lineStartOff)}
+^`)
+  } else if(e.expr) {
+    console.error(e.stack)
+    console.error(e.expr)
   } else {
-    throw e;
+    throw e
   }
 }
 */
