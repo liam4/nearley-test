65eafa7acdd7dcf0dbe22eb2925e0561b12cdcdc
Alex Bates <16batesa@gmail.com>
2016-04-24 08:33:07 +0100

    Better tulun in the shell

--- tulun.js
+++ tulun.js
@@ -1,46 +1,39 @@
 #!/usr/bin/env node
 
-var args = process.argv.slice(2)
+'use strict'
+
+const args = process.argv.slice(2)
 
 if(args.length === 1) {
-  main(args[0])
-} else if(args.length === 2 && args[0] === 'dev') {
-  var spawn = require('child_process').spawn
-  var build = spawn('gulp', ['build'])
-  build.stdout.on('data', function(d) {
-    process.stdout.write(d.toString())
-  })
-  build.on('exit', function() {
-    main(args[1])
-  })
+  main(args[0], true)
 } else {
-  console.error('Usage: tulun [dev] <file>')
+  console.error('Usage: tulun <file.tul>')
   process.exit()
 }
 
-function main(f) {
-  var fs = require('fs')
+function main(f, again) {
+  const fs = require('fs')
 
-  fs.readFile(f, function(err, data) {
+  fs.readFile(f, 'utf8', function(err, data) {
     if(err) {
-      if(err.errno === -2) {
-        console.error('That program doesn\'t exist!')
-      } else if(err.errno === -21) {
-        console.error('Can\'t run a directory!')
+      if(again) {
+        main(`${f}.tul`, false)
       } else {
-        console.error(err)
+        if(err.errno === -2) {
+          console.error('That program doesn\'t exist!')
+        } else if(err.errno === -21) {
+          console.error('Can\'t run a directory!')
+        } else {
+          console.error(err)
+        }
       }
-      process.exit()
-    }
 
-    try {
-      require('./dist/run').run(data.toString())
-      console.log('Done!')
-    } catch(err) {
-      if (err.stack) {
-        console.error(err.stack)
-      } else {
-        console.error(err)
+      try {
+        require('./dist/run').run(data.toString(), `${process.cwd()}/${require('path').dirname(f)}`)
+      } catch(err) {
+        if(err.stack) console.error(err.stack)
+        else console.error(err)
+        process.exit(1)
       }
     }
   })
