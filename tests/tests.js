c235ec05c6af01fe8811592fe62af5503cc96c66
Liam <liam4@users.noreply.github.com>
2016-04-26 15:25:55 -0300

    New tests (#33)
    Adds a bunch of new tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const equal = require('deep-equal')
 const run = require('../dist/run').run
 const oldLog = console.log
 
@@ -19,18 +20,31 @@ const test = function(code, assume) {
 const checkOut = function(compare) {
   // this should be called like this:
   // test(code, checkOut`compareThis`)
+  // or like this:
+  // test(code, checkOut(compareThese))
 
-  // compare: [ compareThis ]
   return function(result) {
-    // result: [ [ '{Print}', compareThis ] ]
-    return result[0][1] === compare[0]
+    // result: [ [ '{Print}', ...compareThese ] ]
+    return equal(result[0], compare)
   }
 }
 
+console.time('Total tests time')
 try {
   console.log('Basic printing ---')
-  test('print("hello!");', checkOut`hello!`)
 
+  // Test print output
+  test(`print("hello!");`, checkOut`hello!`)
+  // Test print with multiple arguments
+  test(`print("hello", "world");`, checkOut(['hello', 'world']))
+
+  /* Commented this out. It works because of this grammar definition:
+   * _Program -> ...
+   *           | ...
+   *           | Command
+   * So a program can be a single command -- that's why this works.
+   */
+  /*
   try {
     // WHY IS THIS WORKING!?!?!?!?!?!??
     test('print("hello! this should not work!")\n', checkOut`hello! this should not work!`)
@@ -38,20 +52,59 @@ try {
     console.log('Newline as separator doesn\'t work, but it hasn\'t been' +
                 'implemented yet so that\'s okay.')
   }
-  test('print("hello!")', checkOut`hello!`)
-  test("print('single quoted');", checkOut`single quoted`)
+  */
+  // Test single quoted string
+  test(`print('single quoted');`, checkOut`single quoted`)
+  test(`print(concat("foo", "bar"));`, checkOut`foobar`)
 
   console.log('Math ---')
-  test('print(+(3, 4));', checkOut`7`)
-  test('print(-(3, 4));', checkOut`-1`)
-  test('print(*(3, 4));', checkOut`12`)
-  test('print(/(3, 4));', checkOut`0.75`)
-  test('print(+(1.25, 1.755));', checkOut`3.005`)
+  // Test basic math operator functions
+  test(`print(+(3, 4));`, checkOut`7`)
+  test(`print(-(3, 4));`, checkOut`-1`)
+  test(`print(*(3, 4));`, checkOut`12`)
+  test(`print(/(3, 4));`, checkOut`0.75`)
+  // Test decimals
+  test(`print(+(1.25, 1.755));`, checkOut`3.005`)
 
   console.log('If/else ---')
-  test(`if(true, {print("good"); });`, checkOut`good`)
+  // Test basic if
+  test(`
+  if(true, {
+    print("good");
+  });`, checkOut`good`)
+  // Test ifel (if condition do this; otherwise do that)
+  test(`
+  ifel(false, {
+    print("bad");
+  }, {
+    print("good");
+  });`, checkOut`good`);
+  // Test else-code as optional argument to if
+  test(`
+  if(false, {
+    print("bad");
+  }, {
+    print("good");
+  });`, checkOut`good`)
+
+  console.log('Logic and comparison ---')
+  // Test logic operator functions
+  test(`print(and(true, false));`, checkOut`<Boolean false>`)
+  test(`print(or(true, false));`, checkOut`<Boolean true>`)
+  test(`print(not(true));`, checkOut`<Boolean false>`)
+  test(`print(not(false));`, checkOut`<Boolean true>`)
+  // Test comparison operator functions
+  test(`print(eq(10, 20));`, checkOut`<Boolean false>`)
+  test(`print(eq(45, 45));`, checkOut`<Boolean true>`)
+  test(`print(lt(10, 20));`, checkOut`<Boolean true>`)
+  test(`print(lt(70, 30));`, checkOut`<Boolean false>`)
+  test(`print(lt(45, 45));`, checkOut`<Boolean false>`)
+  test(`print(gt(10, 20));`, checkOut`<Boolean false>`)
+  test(`print(gt(70, 30));`, checkOut`<Boolean true>`)
+  test(`print(gt(45, 45));`, checkOut`<Boolean false>`)
 } catch (error) {
   console.log = oldLog
   console.log('\x1b[31m[Errored!]\x1b[0m Error in JS:')
   console.error(error)
 }
+console.timeEnd('Total tests time')
