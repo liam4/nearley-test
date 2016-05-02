87ee7da9e7ce1b915177f1d7ec4997488c091c39
Alex Bates <16batesa@gmail.com>
2016-05-02 17:45:18 +0100

    tulun rebuild for windows users

--- tulun.js
+++ tulun.js
@@ -38,13 +38,13 @@ if (args.length === 1) {
   else if (args[0] === 'version') version()
   else if (args[0] === 'help') {
     console.log(`${chalk.green(TULUN)}
-    ${chalk.bold('Command')}       | ${chalk.bold('Description')}
-  ――――――――――――――――+――――――――――――――――――――――――――――――――
-    ${chalk.cyan('tulun        ')} | enters REPL
-    ${chalk.cyan('tulun')} ${chalk.green('<file> ')} | runs ${chalk.yellow('<file>')}.tul
-    ${chalk.cyan('tulun')} ${chalk.green('help   ')} | this screen, silly
-    ${chalk.cyan('tulun')} ${chalk.green('version')} | outputs version number
-    ${chalk.cyan('tulun')} ${chalk.green('rebuild')} | rebuilds source`)
+  ${chalk.bold('Command')}       | ${chalk.bold('Description')}
+ ―――――――――――――――+―――――――――――――――――――――――――――――――――――――――
+  ${chalk.cyan('tulun        ')} | enters REPL
+  ${chalk.cyan('tulun')} ${chalk.green('<file> ')} | runs ${chalk.yellow('<file>')}.tul
+  ${chalk.cyan('tulun')} ${chalk.green('help   ')} | this screen, silly
+  ${chalk.cyan('tulun')} ${chalk.green('version')} | outputs version number
+  ${chalk.cyan('tulun')} ${chalk.green('rebuild')} | dev only! ${chalk.dim('gulp build')} for windows users`)
     process.exit()
   } else main(args[0], true)
 } else if (args.length === 2 && args[0] === 'rebuild') {
